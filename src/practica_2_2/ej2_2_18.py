029cc0fff8938211f994b0b578cd1ff5e65ea4a4
lkru144 <lkru144@g.educaand.es>
2023-11-06 18:22:55 +0100

    a

--- src/practica_2_2/ej2_2_18.py
+++ src/practica_2_2/ej2_2_18.py
@@ -2,36 +2,37 @@
 Solicitar al usuario que ingrese números enteros positivos y, por cada uno, imprimir la suma de los dígitos que lo componen. La condición de corte es que se ingrese el número -1. Al finalizar, mostrar cuántos de los números ingresados por el usuario fueron números pares.
 """
 
-from ej2_2_14 import getNum
+def getNum():
+    return int(input("Introduce un número entero (o -1 para terminar): "))
 
 def sumaDigitos(num):
     suma = 0
     while num > 0:
+        num = getNum()    
         digito = num % 10
         suma += digito
         num //= 10
+        
     return suma
 
-def cantidadNum():
+def cantidadNum(num):
     cantidad = 0
-    while True:
+    while num != -1:
         num = getNum()
-       
-        if num == -1:
-            break
         par = num % 2
+        
         if par == 0:
             cantidad += 1
-    return mayor
+    return cantidad
 
 def main():
     num = getNum()
-    
-    if num < 0:
-        print("El número ingresado no es positivo.")
-    else:
-        res = sumaDigitos(num)
-        print(f"La suma de los dígitos del número es: {res}")
+    res = sumaDigitos(num)
+    print(f"La suma de los dígitos de todos los números es: {res}")
+
+    cantidad = cantidadNum(num)
+    print(f"La cantidad de números pares ingresados es {cantidad}.")
+
 
 if __name__ == "__main__":
     main()
